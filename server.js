412f48a99bdff7f5df9bd7f1aef43e13fc5a813a
RodrigoVilla101 <rodrigovillalpando5@hotmail.com>
2025-11-04 00:40:04 -0600

    feat: Update server.js to allow access from local network

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 3000;
 // Conectar a la base de datos
 connectDB();
 
-// Iniciar servidor
+/* Iniciar servidor
 app.listen(PORT, () => {
   console.log(` Servidor corriendo en puerto ${PORT}`);
   console.log(` http://localhost:${PORT}`);
+});*/
+
+// Iniciar servidor accesible desde la red local
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
+  console.log(`📱 Accesible desde red local en http://192.168.1.4:${PORT}`);
 });
